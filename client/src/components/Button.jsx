6ca84210dcd1abc68f672b8433d0a4d6961b83ff
Prisha <prishabhasin14@gmail.com>
2025-01-05 00:40:46 +0300

    added button

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = ({ text }) => {
+    return (
+      <button className="px-4 py-1 bg-brand text-text whitespace-nowrap rounded-lg hover:bg-custom_02 transition-all duration-300 shadow-md">
+          {text}
+      </button>
+    )
+  }
+  
+  export default Button
